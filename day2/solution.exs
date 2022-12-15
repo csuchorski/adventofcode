5fc90db0bc9a84077e18a65362eb746c733e5ae3
Cezary Suchorski <cezary.suchorski@gmail.com>
2022-12-15 15:35:34 +0100

    finished day2 part 1 and 2

--- day2/solution.exs
+++ day2/solution.exs
@@ -1,35 +1,53 @@
 defmodule Helper do
-  @first {"A", "B", "C"}
-  @second {"X", "Y", "Z"}
+  @rock_map %{
+    win: "Y",
+    draw: "X",
+    lose: "Z"
+  }
+  @paper_map %{
+    win: "Z",
+    draw: "Y",
+    lose: "X"
+  }
+  @scissors_map %{
+    win: "X",
+    draw: "Z",
+    lose: "Y"
+  }
 
   def calculate([], acc), do: acc
 
-  def calculate([a, b], acc) do
+  def calculate(a, b, acc) do
+    calculate([], acc + calc_result(a, b))
   end
 
-  def calculate([head | tail], acc) do
+  def calculate([[a, b] | tail], acc) do
+    calculate(tail, acc + calc_result(a, choose_move(a, b)))
   end
 
-  #   def check_result(a, b) do
-  #     case {a, b} do
-  #       {?a} ->
-  #         :draw
+  def calc_result("A", "X"), do: 4
+  def calc_result("B", "Y"), do: 5
+  def calc_result("C", "Z"), do: 6
 
-  #       {"A", "Y"} ->
-  #         :loss
-  #     end
-  #   end
-  # end
+  def calc_result("C", "X"), do: 7
+  def calc_result("A", "Y"), do: 8
+  def calc_result("B", "Z"), do: 9
 
-  def check_result("A", "X"), do: :draw
-  def check_result("B", "Y"), do: :draw
-  def check_result("C", "Z"), do: :draw
+  def calc_result(_, "X"), do: 1
+  def calc_result(_, "Y"), do: 2
+  def calc_result(_, "Z"), do: 3
 
-  def check_result("A", "Y"), do: :win
-  def check_result("B", "Z"), do: :win
-  def check_result("C", "X"), do: :win
+  def choose_move("A", "X"), do: @rock_map.lose
+  def choose_move("B", "X"), do: @paper_map.lose
+  def choose_move("C", "X"), do: @scissors_map.lose
 
-  def check_result(_, _), do: :loss
+  def choose_move("A", "Y"), do: @rock_map.draw
+  def choose_move("B", "Y"), do: @paper_map.draw
+  def choose_move("C", "Y"), do: @scissors_map.draw
+
+  def choose_move("A", "Z"), do: @rock_map.win
+  def choose_move("B", "Z"), do: @paper_map.win
+  def choose_move("C", "Z"), do: @scissors_map.win
 end
 
 {:ok, binary} = File.read("./input.txt")
@@ -47,4 +65,10 @@ abc =
     [small_list | acc]
   end)
 
-IO.inspect(abc)
+test = [
+  ["A", "Y"],
+  ["B", "X"],
+  ["C", "Z"]
+]
+
+IO.inspect(Helper.calculate(abc, 0))
