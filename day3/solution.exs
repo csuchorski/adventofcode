44af806b7cca67c071244b999022f9a54ec6c5c6
csuchorski <cezary.suchorski@wp.pl>
2022-12-17 22:33:45 +0100

    Managed to build a correct list of ids

--- day3/solution.exs
+++ day3/solution.exs
@@ -1,4 +1,6 @@
 defmodule Helper do
+  @alphabet String.split("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", "")
+
   def build_id_list(lines) do
     Enum.reduce(lines, [], fn pack, acc ->
       find_duplicate(pack) ++ acc
@@ -6,17 +8,46 @@ defmodule Helper do
   end
 
   def find_duplicate(input) do
-    tab = String.split(input, "", trim: true)
-    duplicate = tab -- Enum.uniq(tab)
+    input = String.split(input, "", trim: true)
+
+    map_set1 =
+      Enum.slice(input, 0, div(length(input), 2))
+      |> MapSet.new()
+
+    map_set2 =
+      Enum.slice(input, div(length(input), 2), div(length(input), 2) + 1)
+      |> MapSet.new()
+
+    MapSet.intersection(map_set1, map_set2) |> MapSet.to_list()
+  end
+
+  def map_id_to_number(id_list) do
+    Enum.map(id_list, fn id_letter ->
+      Enum.find_index(@alphabet, fn alpha_letter -> alpha_letter == id_letter end)
+    end)
   end
 end
 
 {:ok, binary} = File.read("./input.txt")
 
+test = """
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+"""
+
+a = "vJrwpWtwJgWrhcsFMMfFFhFp"
+
 lines =
-  String.split(binary, "\n", trim: true)
+  String.split(test, "\n", trim: true)
   |> Enum.reduce([], fn line, acc ->
     [String.trim(line) | acc]
   end)
 
-IO.inspect(Helper.build_id_list(lines))
+IO.inspect(
+  Helper.build_id_list(lines)
+  # |> Helper.map_id_to_number()
+)
