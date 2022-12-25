c23e8d27d5505d80bc17fda074cdb5e58a158ea2
Cezary Suchorski <cezary.suchorski@gmail.com>
2022-12-25 17:49:15 +0100

    day5 moving blocks between stacks works

--- day5/solution.exs
+++ day5/solution.exs
@@ -1,7 +1,5 @@
 defmodule Helper do
-  def split_by_lines(path) do
-    {:ok, binary} = File.read(path)
-
+  def split_by_lines(binary) do
     String.split(binary, "\n", trim: true)
     |> Enum.reduce([], fn line, acc ->
       [String.trim(line) | acc]
@@ -21,21 +19,54 @@ defmodule Helper do
       ]
     end)
   end
+
+  def move_elem([], stacks), do: stacks
+
+  def move_elem([{count, from, to} | tail], stacks) do
+    changed_stacks =
+      for n <- 1..count, reduce: stacks do
+        stacks ->
+          {val, stack_from} = List.pop_at(Enum.at(stacks, from - 1), 0)
+          stack_to = List.insert_at(Enum.at(stacks, to - 1), 0, val)
+
+          List.replace_at(stacks, from - 1, stack_from)
+          |> List.replace_at(to - 1, stack_to)
+      end
+
+    move_elem(tail, changed_stacks)
+  end
 end
 
-stacks = %{
-  1 => ["Q", "H", "C", "T", "N", "S", "V", "B"],
-  2 => ["G", "B", "D", "W"],
-  3 => ["B", "Q", "S", "T", "R", "W", "F"],
-  4 => ["N", "D", "J", "Z", "S", "W", "G", "L"],
-  5 => ["F", "V", "D", "P", "M"],
-  6 => ["J", "W", "F"],
-  7 => ["V", "J", "B", "Q", "N", "L"],
-  8 => ["N", "S", "Q", "J", "C", "R", "T", "G"],
-  9 => ["M", "D", "W", "C", "Q", "S", "J"]
-}
+stacks = [
+  ["Q", "H", "C", "T", "N", "S", "V", "B"],
+  ["G", "B", "D", "W"],
+  ["B", "Q", "S", "T", "R", "W", "F"],
+  ["N", "D", "J", "Z", "S", "W", "G", "L"],
+  ["F", "V", "D", "P", "M"],
+  ["J", "W", "F"],
+  ["V", "J", "B", "Q", "N", "L"],
+  ["N", "S", "Q", "J", "C", "R", "T", "G"],
+  ["M", "D", "W", "C", "Q", "S", "J"]
+]
+
+test_stacks = [
+  ["N", "Z"],
+  ["D", "C", "M"],
+  ["P"]
+]
+
+test_binary = """
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+"""
+
+path = "input.txt"
+{:ok, binary} = File.read(path)
 
 IO.inspect(
-  Helper.split_by_lines("input.txt")
+  Helper.split_by_lines(binary)
   |> Helper.split_to_tuples()
+  |> Helper.move_elem(stacks)
 )
