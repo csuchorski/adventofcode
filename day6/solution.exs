358674906d71a9cf2270ad6e19e28d7bd95dea6f
Cezary Suchorski <cezary.suchorski@gmail.com>
2022-12-25 18:43:22 +0100

    Finished day 6

--- day6/solution.exs
+++ day6/solution.exs
@@ -10,14 +10,24 @@ defmodule Helper do
       count
     end
   end
+
+  def check_letter_uniq_pt2([first | tail] = letters, count) do
+    first_14_chars = Enum.slice(letters, 0..13)
+
+    if Enum.count(Enum.uniq(first_14_chars)) != 14 do
+      check_letter_uniq_pt2(tail, count + 1)
+    else
+      count
+    end
+  end
 end
 
 path = "input.txt"
 {:ok, binary} = File.read(path)
 
-test_binary = "nppdvjthqldpwncqszvftbrmjlhg"
+test_binary = "bvwbjplbgvbhsrlpgdmjqwftvncz"
 
 IO.inspect(
   Helper.split_to_letters(binary)
-  |> Helper.check_letter_uniq(4)
+  |> Helper.check_letter_uniq_pt2(14)
 )
