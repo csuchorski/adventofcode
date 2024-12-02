2d5006cdd43b51eda6edb214c02617589b497371
csuchorski <cezary.suchorski@gmail.com>
2024-12-03 00:04:54 +0100

    day1 part2

--- 2024/day1/solution1.py
+++ 2024/day1/solution1.py
@@ -1,17 +1,36 @@
 #%%
+# left = []
+# right = []
+# with open("input1.txt", "r") as f:
+#     lines = f.readlines()
+
+# for line in lines:
+#     a, b = line.split()
+#     left.append(int(a))
+#     right.append(int(b))
+# left.sort()
+# right.sort()
+# res = 0
+# for i in range(len(left)):
+#     res += abs(left[i] - right[i])
+
+# print(res)
+
+from collections import defaultdict
+
 left = []
-right = []
+right_count = defaultdict(int)
+res = 0
+
 with open("input1.txt", "r") as f:
     lines = f.readlines()
 
 for line in lines:
     a, b = line.split()
     left.append(int(a))
-    right.append(int(b))
-left.sort()
-right.sort()
-res = 0
-for i in range(len(left)):
-    res += abs(left[i] - right[i])
+    right_count[int(b)] += 1
+
+for num in left:
+    res += num * right_count[num]
 
 print(res)
